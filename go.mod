2a938a3f2383225439fe420b6caa1c7f73a4b3e4
Josh Williams <josh.t.williams@outlook.com>
2021-10-27 23:19:30 -0700

    testing

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/joshwi/go-scripts/collector
+
+go 1.16
+
+require (
+	github.com/joshwi/go-plugins/graphdb v0.0.0-20211028023758-f5c972ff6dfb 
+	github.com/joshwi/go-utils v0.0.0-20211018063909-522cad861d2e 
+	github.com/neo4j/neo4j-go-driver v1.8.3 
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"sync"
+
+	"github.com/joshwi/go-plugins/graphdb"
+	"github.com/joshwi/go-utils/parser"
+	"github.com/joshwi/go-utils/utils"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+func main() {
+
+	// Init flag values
+	var query string
+	var name string
+
+	// Define flag arguments for the application
+	flag.StringVar(&query, `q`, ``, `Specify config. Default: <empty>`)
+	flag.StringVar(&name, `c`, `pfr_team_season`, `Specify config. Default: pfr_team_season`)
+	flag.Parse()
+
+	// Pull in env variables: username, password, uri
+	username := utils.Env("NEO4J_USERNAME")
+	password := utils.Env("NEO4J_PASSWORD")
+	host := utils.Env("NEO4J_SERVICE_HOST")
+	port := utils.Env("NEO4J_SERVICE_PORT")
+
+	// Create application session with Neo4j
+	uri := "bolt://" + host + ":" + port
+	driver := graphdb.Connect(uri, username, password)
+	sessionConfig := neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite}
+	session, err := driver.NewSession(sessionConfig)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Find parsing config requested by user
+	config := parser.Config{Name: "", Urls: []string{}, Params: []string{}, Parser: []parser.Parser{}}
+
+	for _, item := range parser.CONFIG_LIST {
+		if name == item.Name {
+			config = item
+		}
+	}
+
+	// Compile parser config into regexp
+	config.Parser = parser.Compile(config.Parser)
+
+	// Grab input parameters from  Neo4j
+	inputs := [][]parser.Tag{{parser.Tag{Name: "name", Value: config.Name}}}
+
+	if len(query) > 0 {
+		inputs = graphdb.RunCypher(session, query)
+	}
+
+	log.Println("COLLECTION - START")
+
+	var wg sync.WaitGroup
+
+	for _, entry := range inputs {
+
+		wg.Add(1)
+
+		go graphdb.RunScript(driver, entry, config, &wg)
+
+	}
+
+	wg.Wait()
+
+	log.Println("COLLECTION - DONE")
+
+	// session.Close()
+
+}
