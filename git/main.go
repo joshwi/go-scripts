7ef66045398e4085959d92ea9fe68fbb3ccf18a9
Josh Williams <josh.t.williams@outlook.com>
2022-01-12 01:39:31 -0800

    removing git for now

--- git/main.go
+++ git/main.go
@@ -1,75 +1,75 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"time"
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"time"
 
-	"github.com/joshwi/go-git/gitscm"
-	"github.com/joshwi/go-utils/utils"
-)
+// 	"github.com/joshwi/go-git/gitscm"
+// 	"github.com/joshwi/go-utils/utils"
+// )
 
-var (
-	DIRECTORY = utils.Env("DIRECTORY")
-	GIT_URL   = utils.Env("GIT_URL")
-	GIT_USER  = utils.Env("GIT_USER")
-	GIT_TOKEN = utils.Env("GIT_TOKEN")
-	GIT_EMAIL = utils.Env("GIT_EMAIL")
-)
+// var (
+// 	DIRECTORY = utils.Env("DIRECTORY")
+// 	GIT_URL   = utils.Env("GIT_URL")
+// 	GIT_USER  = utils.Env("GIT_USER")
+// 	GIT_TOKEN = utils.Env("GIT_TOKEN")
+// 	GIT_EMAIL = utils.Env("GIT_EMAIL")
+// )
 
-func main() {
+// func main() {
 
-	// Init flag values
-	var name string
+// 	// Init flag values
+// 	var name string
 
-	// Define flag arguments for the application
-	flag.StringVar(&name, `r`, `nfldb-backup`, `Specify repository. Default: nfldb-backup`)
-	flag.Parse()
+// 	// Define flag arguments for the application
+// 	flag.StringVar(&name, `r`, `nfldb-backup`, `Specify repository. Default: nfldb-backup`)
+// 	flag.Parse()
 
-	day := time.Now().Format("2006-01-02")
-	directory := fmt.Sprintf("%v/%v", DIRECTORY, name)
-	url := fmt.Sprintf("%v/%v/%v.git", GIT_URL, GIT_USER, name)
+// 	day := time.Now().Format("2006-01-02")
+// 	directory := fmt.Sprintf("%v/%v", DIRECTORY, name)
+// 	url := fmt.Sprintf("%v/%v/%v.git", GIT_URL, GIT_USER, name)
 
-	log.Println(day, directory, url)
+// 	log.Println(day, directory, url)
 
-	project := gitscm.Project{
-		Name:      name,
-		Directory: directory,
-		Url:       url,
-		Token:     GIT_TOKEN,
-		User:      GIT_USER,
-		Email:     GIT_EMAIL,
-	}
+// 	project := gitscm.Project{
+// 		Name:      name,
+// 		Directory: directory,
+// 		Url:       url,
+// 		Token:     GIT_TOKEN,
+// 		User:      GIT_USER,
+// 		Email:     GIT_EMAIL,
+// 	}
 
-	_, err := os.Stat(directory)
-	if os.IsNotExist(err) {
-		project, err = project.Clone(directory)
-	} else {
-		project, err = project.Open(directory)
-	}
+// 	_, err := os.Stat(directory)
+// 	if os.IsNotExist(err) {
+// 		project, err = project.Clone(directory)
+// 	} else {
+// 		project, err = project.Open(directory)
+// 	}
 
-	branches, err := project.Branches(name)
+// 	branches, err := project.Branches(name)
 
-	log.Println(branches)
+// 	log.Println(branches)
 
-	err = project.Branch(name, day)
+// 	err = project.Branch(name, day)
 
-	branches, err = project.Branches(name)
+// 	branches, err = project.Branches(name)
 
-	log.Println(branches)
+// 	log.Println(branches)
 
-	err = utils.Write(directory, "nfl.txt", "chiefs\nchargers\nraiders\nbroncos\n", 0777)
+// 	err = utils.Write(directory, "nfl.txt", "chiefs\nchargers\nraiders\nbroncos\n", 0777)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	err = project.Add()
+// 	err = project.Add()
 
-	err = project.Commit(fmt.Sprintf("DB backup: %v", time.Now().Format(time.RFC3339)))
+// 	err = project.Commit(fmt.Sprintf("DB backup: %v", time.Now().Format(time.RFC3339)))
 
-	err = project.Push()
+// 	err = project.Push()
 
-}
+// }
--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.16
 
 replace github.com/joshwi/go-scripts/collector => ./collector
 
-replace github.com/joshwi/go-scripts/git => ./git
+// replace github.com/joshwi/go-scripts/git => ./git
 
 require (
-	github.com/joshwi/go-git v1.0.0
+	// github.com/joshwi/go-git v1.0.0
 	github.com/joshwi/go-plugins v1.0.2
 	github.com/joshwi/go-utils v1.0.7
 	github.com/neo4j/neo4j-go-driver v1.8.3
