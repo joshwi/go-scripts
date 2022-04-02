131d2d0a3acdf3804a6539ba077620096a768a12
Josh Williams <josh.t.williams@outlook.com>
2022-04-02 02:37:08 -0700

    fixing audit script

--- audit/main.go
+++ audit/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"archive/zip"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/joshwi/go-utils/utils"
 )
@@ -19,97 +15,27 @@ var (
 	PASSWORD  = os.Getenv("NEO4J_PASSWORD")
 	HOST      = os.Getenv("NEO4J_SERVICE_HOST")
 	PORT      = os.Getenv("NEO4J_SERVICE_PORT")
-)
-
-var audits = map[string][]utils.Tag{
-	"nfl": {
-		{Name: "games", Value: "MATCH (n:games) RETURN n.label as label ORDER BY label"},
-		{Name: "colors", Value: "MATCH (n:colors) RETURN n.label as label ORDER BY label"},
-	},
-}
-
-func Unzip(src, dest string) error {
-
-	response := fmt.Sprintf(`[ Function: Unzip ] [ Source: %v ] [ Destination: %v ] [ Status: Success ]`, src, dest)
-
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	os.MkdirAll(dest, 0777)
-
-	// Closure to address file descriptors issue with all the deferred .Close() methods
-	extractAndWriteFile := func(f *zip.File) error {
-		log.Println(f.Name)
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		log.Println("ILIANA")
-
-		path := filepath.Join(dest, f.Name)
-
-		// Check for ZipSlip (Directory traversal)
-		if !strings.HasPrefix(path, filepath.Clean(dest)+string(os.PathSeparator)) {
-			return fmt.Errorf("illegal file path: %s", path)
-		}
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, f.Mode())
-		} else {
-			os.MkdirAll(filepath.Dir(path), f.Mode())
-			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	for _, f := range r.File {
-		err := extractAndWriteFile(f)
-		if err != nil {
-			response = fmt.Sprintf(`[ Function: Unzip ] [ Source: %v ] [ Destination: %v ] [ Status: Failed ] [ Error: %v ]`, src, dest, err)
-			log.Println(response)
-			return err
-		}
-	}
-
-	return nil
-}
-
-func main() {
 
 	// Init flag values
-	var repo string
-	var collection string
+	repo       string
+	collection string
 
+	audits = map[string][]utils.Tag{
+		"nfl": {
+			{Name: "games", Value: "MATCH (n:games) RETURN n.label as label ORDER BY label"},
+			{Name: "colors", Value: "MATCH (n:colors) RETURN n.label as label ORDER BY label"},
+		},
+	}
+)
+
+func init() {
 	// Define flag arguments for the application
 	flag.StringVar(&repo, `r`, `deltadb-backup`, `Specify config. Default: nfldb-backup`)
 	flag.StringVar(&collection, `a`, `nfl`, `Specify collection audit. Default: nfl`)
 	flag.Parse()
+}
+
+func main() {
 
 	// Create application session with Neo4j
 	// uri := "bolt://" + HOST + ":" + PORT
@@ -125,7 +51,7 @@ func main() {
 	// audit := audits[collection]
 	directory := fmt.Sprintf("%v/%v/%v", DIRECTORY, repo, collection)
 
-	err := Unzip(directory+".zip", directory)
+	err := utils.Unzip(directory+".zip", directory)
 
 	if err != nil {
 		log.Fatal(err)
